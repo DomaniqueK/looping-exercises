6b08991d37dc99e01d06fcca343285a331ec6551
DomaniqueK <domaniqueking24@gmail.com>
2025-10-03 13:09:28 -0500

    Created a whileloop to display "I love Java" 5 times.

---
+++ src/main/java/WhileLoop.java
@@ -0,0 +1,12 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int count = 1;
+        int sum = 0;
+                while (count <= 5) {
+                    count++;
+
+                    System.out.println("I love Java");
+                }
+    }
+}
